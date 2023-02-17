cfbe93cbdfebe8062c2841b462c1069a4ab746f8
Vander Luis Lauschner <vander.lauschner@proway.treina>
2023-02-16 21:12:03 -0300

    subindo primeiras automatizações

---
+++ robo01.py
@@ -0,0 +1,8 @@
+#importando a biblioteca pyautogui e atribuindo um apelido direto
+import pyautogui as p
+
+#usando p para criar um time de execucao
+p.sleep(5)
+
+#print position ira mostrar as coordenadas
+print(p.position())
---
+++ robo02.py
@@ -0,0 +1,42 @@
+import pyautogui as p
+
+p.moveTo(x=42, y=350, duration=1)
+p.doubleClick(x=42, y=350)
+
+p.moveTo(x=273, y=17, duration=1)
+p.click(x=273, y=17)
+
+p.moveTo(x=161, y=52, duration=1)
+p.click(x=161, y=52)
+p.typewrite('Renal Vida')
+p.hotkey('enter')
+
+p.moveTo(x=294, y=351, duration=1)
+p.click(x=294, y=351)
+
+p.moveTo(x=650, y=124, duration=1)
+p.sleep(2)
+p.click(x=650, y=124)
+
+p.moveTo(x=1505, y=748, duration=1)
+p.sleep(2)
+p.click(x=1505, y=748)
+
+p.moveTo(x=118, y=378, duration=1)
+p.click(x=118, y=378)
+p.typewrite('Vander Luis Lauschner')
+p.moveTo(x=118, y=429, duration=1)
+p.click(x=118, y=429)
+p.typewrite('vanderlaus@hotmail.com')
+p.moveTo(x=425, y=425, duration=1)
+p.click(x=425, y=425)
+p.typewrite('14041981')
+p.moveTo(x=125, y=474, duration=1)
+p.click(x=125, y=474)
+p.typewrite('47991152180')
+p.moveTo(x=122, y=638, duration=1)
+p.click(x=122, y=638)
+p.moveTo(x=619, y=739, duration=1)
+p.click(x=619, y=739)
+
+
